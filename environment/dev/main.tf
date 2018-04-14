7781ad1a0f7276fe8acfdb4954d3f7b993606236
于鑫蕾 <xinlei.yu@capgemini.com>
2018-04-14 11:17:59 +0800

    rename variables

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -2,16 +2,22 @@
 # NOTE: if you defined these values as environment variables, you do not have to include this block
 provider "azurerm" {
 
-version = "~> 1.3.2" 
+version = "~> 0.3.3" 
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
   tenant_id       = "${var.tenant_id}"
-  environment     = "${var.environment}"
+  environment     = "${var.rm_env}"
 }
 
 # create a resource group 
 resource "azurerm_resource_group" "microservice" {
     name = "${var.resource_group_name}"
     location = "${var.location}"
+    tags {
+        environment = "${var.resource_env}"
+    }
 }
+
+
+
--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -2,6 +2,7 @@ variable subscription_id {}
 variable tenant_id {}
 variable client_id {}
 variable client_secret {}
-variable environment {}
+variable rm_env {}
+variable resource_env {}
 variable location {}
 variable resource_group_name {}
